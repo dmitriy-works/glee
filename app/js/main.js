62ab008f29fad2c9bf52929c7041cbc47bf81185
dkhimich <dmitriy-works@ukr.net>
2024-03-08 19:39:28 +0300

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function () {
 
+
+  $('.product-detalis__thumb').slick({
+      asNavFor: '.product-detalis__big',
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      focusOnSelect: true,
+      vertical: true,
+  })
+
+  $('.product-detalis__big').slick({
+      asNavFor: '.product-detalis__thumb',
+      arrows: false,
+      draggable: false,
+      fade: true,
+  })
+
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
     $(this).addClass('shop-content__filter-btn--active')
@@ -41,8 +57,7 @@ $('.select-style').styler();
 
 
 
-$('.star').rateYo({ 
-  rating: 5, 
+$('.star').rateYo({    
   starWidth: "17px",
   normalFill: "#ccccce",
   ratedFill: "#ffc35b",
@@ -54,8 +69,7 @@ $('.star').rateYo({
 
 
 
-$('.star-list').rateYo({
-  rating: 5, 
+$('.star-list').rateYo({   
   starWidth: "17px",
   normalFill: "#ccccce",
   ratedFill: "#ffc35b",
@@ -64,6 +78,17 @@ $('.star-list').rateYo({
   numStars: 5,
   starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
 });
+
+
+$('.product-detalis__star').rateYo({  
+  starWidth: "17px",
+  normalFill: "#ccccce",
+  ratedFill: "#ffc35b",
+  readOnly: true,
+  spacing: "15px",
+  numStars: 5,
+  starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+})
 
 
 
