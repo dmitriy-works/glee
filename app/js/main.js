5035637d53b776cdd2138d08cdb5192672d03a38
dkhimich <dmitriy-works@ukr.net>
2024-03-13 18:04:24 +0300

    added tab-1 Все о товаре

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,55 @@
 $(function () {
 
 
+  $('.related-slider__items').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="related-slider__arrow slick-prev"><img src="images/icons/left-arrow-long.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="related-slider__arrow slick-next"><img src="images/icons/right-arrow-long.svg" alt="arrow-right"></button>',
+    responsive: [
+    {
+      breakpoint: 1251,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1, 
+              
+      }
+    },
+    {
+      breakpoint: 861,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+  });
+
+  
+
+
+
+  $('.product-tabs__item').on('click', function (e) {
+    e.preventDefault()
+    $('.product-tabs__item').removeClass('product-tabs__item--active');
+    $(this).addClass('product-tabs__item--active')
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active')
+
+  })
+
+
   $('.product-detalis__thumb').slick({
       asNavFor: '.product-detalis__big',
       slidesToShow: 3,
@@ -32,9 +81,7 @@ $(function () {
   })
 
 
-$('.select-style').styler();
-
-
+$('.select-style, .product-item__num').styler();
 
    
   $('.filter-price__input').ionRangeSlider({
@@ -57,17 +104,6 @@ $('.select-style').styler();
 
 
 
-$('.star').rateYo({    
-  starWidth: "17px",
-  normalFill: "#ccccce",
-  ratedFill: "#ffc35b",
-  readOnly: true,
-  spacing: "3px",
-  numStars: 5, 
-  starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
-});
-
-
 
 $('.star-list').rateYo({   
   starWidth: "17px",
@@ -80,32 +116,27 @@ $('.star-list').rateYo({
 });
 
 
-$('.product-detalis__star').rateYo({  
-  starWidth: "17px",
+$('.product-detalis__star').rateYo({    
+  starWidth: "13px",
   normalFill: "#ccccce",
   ratedFill: "#ffc35b",
   readOnly: true,
-  spacing: "15px",
+  spacing: "3px",
   numStars: 5,
   starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
 })
 
 
+$('.star').rateYo({         
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+  })
 
 
-
-// $('.star').rateYo({
-//     rating: 5,
-//     starWidth: "14px",
-//     normalFill: "#7b7b7b",
-//     ratedFill: "#ffc35b",
-//     readOnly: true,
-//     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
-//   });
-
-
-  $('.filter-recent__item-star').rateYo({
-    
+  $('.filter-recent__item-star').rateYo({    
     starWidth: "11px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
@@ -113,8 +144,18 @@ $('.product-detalis__star').rateYo({
     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
   })
       
-});
+
 
 $('.burger-menu').on('click', function () {
   $('.menu__list').toggleClass('menu__list--active');
 })
+
+
+
+
+
+
+
+
+})
+  
