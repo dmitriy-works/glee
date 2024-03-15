6ce8f6ed0d44d4e57b916d660efe679b1bd644ac
dkhimich <dmitriy-works@ukr.net>
2024-03-15 21:32:55 +0300

    added tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,49 @@
 $(function () {
 
 
+const likeBtn = document.querySelector('.btn-like')
+const dislikeBtn = document.querySelector('.btn-dislike')
+const sokPlus = document.querySelector('.sok-plus')
+const sokMinus = document.querySelector('.sok-minus')
+
+likeBtn.addEventListener('click',like)
+dislikeBtn.addEventListener('click',dislike)
+
+let c = 0;
+let b = 0;
+
+function like() {
+  c ++
+
+  sokPlus.innerHTML = `${c}`;
+}
+
+function dislike() {
+  b ++
+
+  sokMinus.innerHTML = `${b}`;
+}
+
+
+document.getElementById("feedback-btn").addEventListener("click", function () {
+    document.getElementById("feedback-modal").classList.add("feedback__modal-open")
+  });
+
+  document.getElementById("modal-close").addEventListener("click", function () {
+    document.getElementById("modal-window").classList.remove("open")
+  });
+
+
+
+  document.getElementById("open-modal-btn").addEventListener("click", function () {
+    document.getElementById("modal-window").classList.add("open")
+  });
+
+  document.getElementById("modal-close").addEventListener("click", function () {
+    document.getElementById("modal-window").classList.remove("open")
+  });
+  
+
   $('.related-slider__items').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -141,6 +184,16 @@ $('.star').rateYo({
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00",
     spacing: "7px",
+    readOnly: true,
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
+  })
+
+  $('.product-feedback__star').rateYo({
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "7px",
+    readOnly: true,
     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>'
   })
       
